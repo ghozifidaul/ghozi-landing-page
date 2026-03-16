9e77ac093ba841ce7a0db4a665e356178aec009e
Ghozi Fidaul <ghozifidaul@gmail.com>
2026-03-17 06:26:43 +0700

    set custom avatar as site favicon and apple-touch-icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/webp" href="/ghozifidaul.webp" />
+        <link rel="apple-touch-icon" href="/ghozifidaul.webp" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
